0a278c4451ed31ac0105a2b4248a55148e31177b
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-03-23 17:14:43 +0300

    Signleton Design Pattern is Implemented

---
+++ DesignPatterns.Singleton/EmailHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    internal class EmailHelper
+    {
+        private static EmailHelper instance;
+
+        private EmailHelper() { }
+
+        public static EmailHelper Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new EmailHelper();
+                }
+
+                return instance;
+            }
+        }
+
+        public void SendEmail(string message)
+        {
+            Console.WriteLine(message + " is sent.");
+        }
+    }
+}
---
+++ DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailHelper.Instance.SendEmail("Example Message");
+        }
+    }
+}
